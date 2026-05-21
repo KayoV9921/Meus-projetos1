c8fe59cb294eca7c509b9d614554df7d73d456f7
Kayo Veloso <kaioplay260@gmail.com>
2026-05-21 09:29:28 -0300

    Create main.c

---
+++ exerc.21/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char palavra[100];
+    int i;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra);
+
+    printf("Palavra invertida: ");
+
+    for(i = strlen(palavra) - 1; i >= 0; i--) {
+        printf("%c", palavra[i]);
+    }
+
+    return 0;
+}
